7bb2d93875268468d2c1290822e03d4fa3ebd2b9
marcos3425 <mv3541906@gmail.com>
2025-05-09 16:31:06 -0300

    Update index.php

--- atividade2/index.php
+++ atividade2/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Minha Página (Fernando)</title>
+    <title>Minha Página</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
